778bd4e3a1e4663e765b669be2e6f7ef914c028f
Quentin <quentin.chevy@gmail.com>
2024-01-12 11:10:34 -0500

    same as previous

--- BpodParam_PCdep.m
+++ BpodParam_PCdep.m
@@ -11,30 +11,30 @@ switch getenv('computername')
         Param.rig='Photometry2';
         Param.nidaqDev='Dev2';
         Param.LED1Amp=2;
-        Param.LED2Amp=3.9;
+        Param.LED2Amp=2.5;
         Param.LED1bAmp=2;
     case 'KEPECS-PHOTO03'
         Param.rig='Photometry3';
         Param.nidaqDev='Dev1';
         Param.LED1Amp=2;
-        Param.LED2Amp=5;
+        Param.LED2Amp=4;
         Param.LED1bAmp=2;
 	case 'TQ_TOP'
         Param.rig='Photometry4';
         Param.nidaqDev='Dev1';
-        Param.LED1Amp=0.55;
+        Param.LED1Amp=0.68;
         Param.LED2Amp=5;
         Param.LED1bAmp=2;
     case 'TQ_MIDDLE'
         Param.rig='Photometry5';
         Param.nidaqDev='Dev2';
-        Param.LED1Amp=0.55;
+        Param.LED1Amp=1.7;
         Param.LED2Amp=5;
         Param.LED1bAmp=2;
     case 'TQ_BOTTOM'
         Param.rig='Photometry6';
         Param.nidaqDev='Dev1';
-        Param.LED1Amp=0.55;
+        Param.LED1Amp=1.5;
         Param.LED2Amp=5;
         Param.LED1bAmp=2;
     otherwise

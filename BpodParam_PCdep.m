a1c12913e335f9783e129a5ac264033d419c0702
Quentin <quentin.chevy@gmail.com>
2024-01-16 15:01:15 -0500

    adding PulsePal

--- BpodParam_PCdep.m
+++ BpodParam_PCdep.m
@@ -7,42 +7,56 @@ switch getenv('computername')
         Param.LED1Amp=2;
         Param.LED2Amp=2.8;
         Param.LED1bAmp=2;
+        Param.PPCOM='COM4';
+        Param.BPPP_BNC=2;
     case 'KEPECS-PHOTO02'
         Param.rig='Photometry2';
         Param.nidaqDev='Dev2';
         Param.LED1Amp=2;
         Param.LED2Amp=2.5;
         Param.LED1bAmp=2;
+        Param.PPCOM='COM4';
+        Param.BPPP_BNC=2;
     case 'KEPECS-PHOTO03'
         Param.rig='Photometry3';
         Param.nidaqDev='Dev1';
         Param.LED1Amp=2;
         Param.LED2Amp=4;
         Param.LED1bAmp=2;
+        Param.PPCOM='COM4';
+        Param.BPPP_BNC=2;
 	case 'TQ_TOP'
         Param.rig='Photometry4';
         Param.nidaqDev='Dev1';
         Param.LED1Amp=0.68;
         Param.LED2Amp=5;
         Param.LED1bAmp=2;
+        Param.PPCOM='COM6';
+        Param.BPPP_BNC=2;
     case 'TQ_MIDDLE'
         Param.rig='Photometry5';
         Param.nidaqDev='Dev2';
         Param.LED1Amp=1.7;
         Param.LED2Amp=5;
         Param.LED1bAmp=2;
+        Param.PPCOM='COM4';
+        Param.BPPP_BNC=2;
     case 'TQ_BOTTOM'
         Param.rig='Photometry6';
         Param.nidaqDev='Dev1';
         Param.LED1Amp=1.5;
         Param.LED2Amp=5;
         Param.LED1bAmp=2;
+        Param.PPCOM='COM5';
+        Param.BPPP_BNC=2;
     otherwise
         Param.rig='Unknown';
         Param.nidaqDev='Dev1';
         Param.LED1Amp=0;
         Param.LED2Amp=0;
         Param.LED1bAmp=0;
+        Param.PPCOM='COM6';
+        Param.BPPP_BNC=2;
         disp('Unrecognized computer - register possible in BpodParam_PCdep function')
 end
 end

4b50a2522892941d2264c012c7df3e9b21f7f7f5
Quentin <quentin.chevy@gmail.com>
2025-08-11 12:45:47 -0400

    Pairing 20 to 50 trials

--- Pulsepal_Stim/Bpod_GUI_StimPairing.m
+++ Pulsepal_Stim/Bpod_GUI_StimPairing.m
@@ -2,10 +2,10 @@ function thisGUI=Bpod_GUI_StimPairing(thisGUI,thisProtocol)
 
     switch thisProtocol
         case 'AuditoryTuning'
-            thisGUI.Repetition=20;
+            thisGUI.Repetition=50;
             thisGUI.PureTones=0;
             thisGUI.WhiteNoise=0;
         case 'VisualTuning'
-            thisGUI.Repetition=20;
+            thisGUI.Repetition=50;
     end
 end

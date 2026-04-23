95a909e7809afeb0a209c771715a80b5103d9c03
Quentin <quentin.chevy@gmail.com>
2026-04-23 15:13:14 +0200

    new bpodpath function for new bpod version

---
+++ BpodPath_v2.m
@@ -0,0 +1,47 @@
+function BpodPath_v2(varargin)
+%%Adjust bpod folders - data and protocol - to make it user-specific.
+%% Designed by Quentin 2026 for PBI ePHYS plateform
+global BpodSystem
+%% Default Bpod path
+defaultBpodPath='E:\Bpod Local\';
+defaultUserPath='E:\Bpod-Data\';
+
+%% Inputs
+% Parse inputs from varargin
+p = inputParser();  
+% Define expected parameters with defaults
+addParameter(p, 'username',     []);
+addParameter(p, 'userpath',    []);
+
+% Parse the inputs
+parse(p, varargin{:});
+username    = p.Results.username;
+userpath   =  p.Results.userpath;
+
+
+%% Build Bpod path
+if ~isempty(username)
+    if isempty(userpath)
+        bpodpath=[defaultUserPath username filesep];
+    else
+        bpodpath=[userpath filesep username filesep];
+    end
+    if ~exist(bpodpath,'dir')
+        disp('Bpod user does not exist, check BpodPath_v2')
+        bpodpath=defaultBpodPath;
+    end
+else
+    bpodpath=defaultBpodPath;
+end
+
+bpodpath_protocols=[bpodpath 'protocols' filesep];
+bpodpath_data=[bpodpath 'data' filesep];
+
+%% Set Bpod path  
+BpodSystem.Path.ProtocolFolder=bpodpath_protocols;
+BpodSystem.Path.DataFolder=bpodpath_data;
+BpodSystem.SystemSettings.ProtocolFolder = bpodpath_protocols;
+BpodSystem.SystemSettings.DataFolder     = bpodpath_data;
+BpodSystem.SaveSettings;
+
+end
